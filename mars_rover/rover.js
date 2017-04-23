c1b6b9faf4c0049b266856672437a50baa29f12e
Javier Armero <Javalba90@gmail.com>
2017-04-23 13:31:09 +0200

    Mars Rover Kata iterations 1 and 2

--- mars_rover/rover.js
+++ mars_rover/rover.js
@@ -1,25 +1,163 @@
 var myRover = {
-  position: [0,0], 
-  direction: 'N'
+  position: [0,0],
+  direction: 'N',
+  moves: 0
 };
 
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-  };
+/*Function that print result in html page and log console. */
+function printResult(rover,movement){
+  rover.moves +=1;
+  var infoMove="";
+  if(movement==="f") infoMove+="Go forward!";
+  if(movement==="b") infoMove+="Go backward!";
+  if(movement==="l") infoMove+="Turn left!";
+  if(movement==="r") infoMove+="Turn right!";
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  // var info = rover.moves+" - "+infoMove+" - Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]\nDirection: "+rover.direction+"<br></br>";
+
+  // var para = document.createElement("p");
+  // var node = document.createTextNode(info);
+  // para.appendChild(node);
+  // var element = document.getElementById("rover-moves");
+  // element.appendChild(para);
+
+  var info = rover.moves+" - "+infoMove+" - Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]\nDirection: "+rover.direction+"<br></br>";
+  document.getElementById("myRover").insertAdjacentHTML('beforebegin', info);
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]\nNew Direction: "+rover.direction);
 }
 
-goForward(myRover);
+/*Move rover forward one position
+*/
+function goForward(rover) {
+  var movement="f";
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]++;
+      break;
+    case 'E':
+      rover.position[1]++;
+      break;
+    case 'S':
+      rover.position[0]--;
+      break;
+    case 'W':
+      rover.position[1]--;
+      break;
+  }
+  validations(rover,movement);
+  printResult(rover,movement);
+}
+
+/*Move rover backward one position*/
+function goBackward(rover) {
+  var movement="b";
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--;
+      break;
+    case 'E':
+      rover.position[1]--;
+      break;
+    case 'S':
+      rover.position[0]++;
+      break;
+    case 'W':
+      rover.position[1]++;
+      break;
+  }
+  printResult(rover,movement);
+}
+
+/*Turn rover right. Without moves.*/
+function turnRight(rover) {
+  var movement="r";
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'S';
+      break;
+    case 'S':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'N';
+      break;
+  }
+  printResult(rover,movement);
+}
+
+/*Turn rover left. Without moves.*/
+function turnLeft(rover) {
+  var movement="l";
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'E':
+      rover.direction = 'N';
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+  }
+printResult(rover,movement);
+}
+
+/*
+* User orders given. Format example: ffrffls. Max 15 commands
+*/
+function userOrders() {
+
+    /*Catch user input into var orders.*/
+		var orders = document.getElementById("userInput").value;
+
+    /*Convert orders to an array*/
+    var arrayOrders = orders.split("");
+
+    for (var i = 0; i < arrayOrders.length; i++) {
+      switch(arrayOrders[i]) {
+        case 'f':
+          goForward(myRover);
+          break;
+        case 'b':
+          goBackward(myRover);
+          break;
+        case 'r':
+          turnRight(myRover);
+          break;
+        case 'l':
+          turnLeft(myRover);
+          break;
+      }
+    }
+}
+
+/*
+* Set Mars Rover to initial values.
+*/
+function resetRover() {
+  myRover.position = [0,0];
+  myRover.direction ='N';
+  myRover.moves = 0;
+
+  // var myNode = document.getElementById("rover-moves");
+  // while (myNode.firstChild) {
+  //   myNode.removeChild(myNode.firstChild);
+
+}
+
+/*
+* - The rover is on a 10 x 10 grid
+*/
+function validations (rover,movements){
+
+  if(rover.position[0] >= 10) rover.position[0] = 10;
+  if(rover.position[1] >= 10) rover.position[1] = 10;
+
+
+}
